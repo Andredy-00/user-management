18ef5f30f5753cb20e385018311ae2f0df196801
andredy-00 <andredyguerra00@gmail.com>
2026-01-01 19:18:20 -0500

    Feat: Auth

---
+++ src/components/login/ui/AuthForm.tsx
@@ -0,0 +1,14 @@
+import { loginAction, signUpAction } from "@/actions/auth/authAction"
+
+export const LoginForm = () => {
+    return (
+        <form>
+            <label htmlFor="email">Email:</label>
+            <input id="email" name="email" type="email" required />
+            <label htmlFor="password">Password:</label>
+            <input id="password" name="password" type="password" required />
+            <button formAction={loginAction}>Log in</button>
+            <button formAction={signUpAction}>Sign up</button>
+        </form>
+    )
+}
